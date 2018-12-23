5e56460efdf0726218adfb16e2dc72c8dba5d9f3
Kraisie <32745233+Kraisie@users.noreply.github.com>
2018-12-23 13:01:59 +0100

    Creating some form of json post request

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 browser.browserAction.onClicked.addListener(clickMe);
-var running = true;
-var interval = setInterval(runFunction, 1000);
+
+//start deactivated
+var running = false;    
+browser.browserAction.setIcon({ path: "pics/crossIcon96.png" });
 
 function clickMe() {
     //turn extension on/off
@@ -21,33 +23,30 @@ function runFunction(tab) {
         .then(function (value) {
             var tabsArray = [];
             for (i = 0; i < value.length; i++) {
-                console.log(value);
                 if (!value[i].active) {
                     tabsArray.push(value[i].url);
                 }
             }
 
-            //get titles of tabs with audio playing
-            var titles = "";
-            for (i = 0; i < value.length; i++) {
-                titles = titles.concat(value[i].title);
-                titles += "\n";
-            }
-            console.log("titles: " + titles);
-
-            //post titles to localhost
+            //JSON.stringify did not work for some reason so generate json 'by hand'
+            var playing = false;
+            var dataJson = "{arrTabs:[";
+            //if audio tabs exist we add them to an array in a json conform string
             if (value.length > 0) {
-                //currently playing
-                var xhr = new XMLHttpRequest();
-                xhr.open("POST", 'http://localhost:6789', true);            //POST request to localhost:6789, NO third party
-                xhr.setRequestHeader('Content-Type', 'text/plain');
-                xhr.send(titles);
-            } else {
-                //nothing playing anymore
-                var xhr = new XMLHttpRequest();
-                xhr.open("POST", 'http://localhost:6789', true);            //POST request to localhost:6789, NO third party
-                xhr.setRequestHeader('Content-Type', 'text/plain');
-                xhr.send(".!.!.NoSound.!.!.");                              //well...
+                playing = true;
+                for (i = 0; i < value.length; i++) {
+                    dataJson = dataJson.concat(value[i].title);
+                    if (i + 1 < value.length) {
+                        dataJson = dataJson.concat(",");
+                    }
+                }
             }
+            dataJson = dataJson.concat("],bPlaying:" + playing + "}");
+
+            //currently playing
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", 'http://localhost:6789', true);            //POST request to localhost:6789, NO third party
+            xhr.setRequestHeader('Content-Type', 'text/plain');         //text/plain as json does not reach my localhost
+            xhr.send(dataJson);
         });
 }
